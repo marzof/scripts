3d65dd67c111840d5c48e032033339bc4e3ac8aa
marzof <marzof@tin.it>
2020-07-19 16:54:34 +0200

    Fix large image bug using pdftoppm to convert pdf
    ...and clean code

--- imagediff.sh
+++ imagediff.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
+FILENAME=${1%.*}-${2%.*};
+## Add this to get file output during the process
+# tee >(convert - $FILENAME.png) |
+
+## #Convert pdf to png (direct convert creates artifacts for large images)
+## Output:
+## -[0] -> first image rasterized
+## -[1] -> second image rasterized
 { 
-## #Convert pdf to png
-	convert -density $3 $1 -flatten -colorspace RGB miff:-; \
-	convert -density $3 $2 -flatten -colorspace RGB miff:-; \
+	pdftoppm -png -rx $3 -ry $3 $1 | convert - -flatten -colorspace RGB miff:-; \
+	pdftoppm -png -rx $3 -ry $3 $2 | convert - -flatten -colorspace RGB miff:-; \
 } |
-## mpr:A[0] -> first image rasterized
-## mpr:A[1] -> second image rasterized
-{ 
+
 ## #Create diff mask and convert to grayscale png
-	convert - -write mpr:AB \
-		-delete 0--1 \
+## Output:
+## mpr:A[0] -> diff mask
+## mpr:A[1] -> first image grayscaled 
+## mpr:A[2] -> second image grayscaled 
+## mpr:A[3] -> first image rasterized
+{ 
+	convert - -write mpr:AB -delete 0--1 \
 		\( mpr:AB[0] mpr:AB[1] -compose difference -composite \) \
 		\( mpr:AB[0] -colorspace gray \) \
 		\( mpr:AB[1] -colorspace gray \) \
 		mpr:AB[0] \
 		miff:-; 
 } |
-## mpr:A[0] -> diff mask
-## mpr:A[1] -> first image grayscaled 
-## mpr:A[2] -> second image grayscaled 
-## mpr:A[3] -> first image rasterized
-{ 
+
 ## #Colorize sources
-	convert - -write mpr:AB \
-		-delete 0--1 \
+## Output:
+## mpr:A[0] -> diff mask
+## mpr:A[1] -> first image colorized (yellow) 
+## mpr:A[2] -> second image colorized (red) 
+## mpr:A[3] -> first image rasterized 
+{ 
+	convert - -write mpr:AB -delete 0--1 \
 		mpr:AB[0] \
 		\( mpr:AB[1] \( mpr:AB[1] -fill '#cc6600' -colorize 100 \) \
 			-compose blend -composite \) \
@@ -33,53 +44,51 @@
 		mpr:AB[3] \
 		miff:-;
 } |
-## mpr:A[0] -> diff mask
-## mpr:A[1] -> first image colorized (yellow) 
-## mpr:A[2] -> second image colorized (red) 
-## mpr:A[3] -> first image rasterized 
-{
+
 ## #Create colorized masks
-	convert - -write mpr:AB \
-		-delete 0--1 \
+## Output:
+## mpr:A[0] -> first image colorized mask
+## mpr:A[1] -> second image colorized mask
+## mpr:A[2] -> first image rasterized 
+## mpr:A[3] -> diff mask
+{
+	convert - -write mpr:AB -delete 0--1 \
 		\( mpr:AB[1] mpr:AB[0] -alpha Off -compose CopyOpacity -composite \) \
 		\( mpr:AB[2] mpr:AB[0] -alpha Off -compose CopyOpacity -composite \) \
 		mpr:AB[3] \
 		mpr:AB[0] \
 		miff:-; 
 } |
-## mpr:A[0] -> first image colorized mask
-## mpr:A[1] -> second image colorized mask
-## mpr:A[2] -> first image rasterized 
-## mpr:A[3] -> diff mask
-{
+
 ## #Multiply diffs
-	convert - -write mpr:AB \
-		-delete 0--1 \
+## Output:
+## mpr:A[0] -> colorized diff
+## mpr:A[1] -> first image rasterized 
+## mpr:A[2] -> diff mask
+{
+	convert - -write mpr:AB -delete 0--1 \
 		\( mpr:AB[1] mpr:AB[0] -compose multiply -composite \) \
 		mpr:AB[2] \
 		mpr:AB[3] \
 		miff:-; 
 } | 
-## mpr:A[0] -> colorized diff
-## mpr:A[1] -> first image rasterized 
-## mpr:A[2] -> diff mask
-{
+
 ## #Clean the source
-	convert - -write mpr:AB \
-		-delete 0--1 \
+## Output:
+## mpr:A[0] -> colorized diff
+## mpr:A[1] -> first image cleaned 
+{
+	convert - -write mpr:AB -delete 0--1 \
 		mpr:AB[0] \
 		\( mpr:AB[1] mpr:AB[2] -compose plus -composite \) \
 		miff:-;
 } |
-## mpr:A[0] -> colorized diff
-## mpr:A[1] -> first image cleaned 
-{
+
 ## #Compose the cleaned source with diff
-	convert - -write mpr:AB \
-		-delete 0--1 \
+{
+	convert - -write mpr:AB -delete 0--1 \
 		\( mpr:AB[1] mpr:AB[0] -composite \) \
 		miff:-;
 } |
-	display
-#	## Save as file:
-#	##	${1%.*}-${2%.*}_diff.png;
+
+display
