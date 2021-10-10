dacfb343934cb9cc618d182e42260b2838918c04
marzof <marzof@tin.it>
2021-10-10 18:24:36 +0200

    Remove prj project: moved to other repository

--- prj.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-~/softwares/blender30/blender --background $1 --python ~/softwares/scripts/addons/prj/main.py -- $2
-#~/softwares/blender30_previous/blender --background $1 --python ~/softwares/scripts/addons/prj/main.py -- $2
