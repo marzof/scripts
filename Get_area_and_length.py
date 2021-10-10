9b9462fce6ae5bc76befbc4f3c1091f61a0268fd
marzof <marzof@tin.it>
2021-10-10 18:23:17 +0200

    Add perimeter calculation in Get_area_and_length.py

--- Get_area_and_length.py
+++ Get_area_and_length.py
@@ -3,6 +3,7 @@ import bmesh
 
 total_area = 0
 total_length = 0
+perimeter = 0
 for obj in bpy.context.selected_objects:
     obj_area = 0
     obj_length = 0
@@ -12,11 +13,15 @@ for obj in bpy.context.selected_objects:
     bm = bmesh.from_edit_mesh(obj.data)
     for edge in bm.edges:
         if edge.select:
+            print('edge', edge.index, edge.is_manifold)
             edge_length = edge.calc_length()
             print(edge.index, edge_length)
             obj_length += edge_length
-    print(obj.name, obj_area)
+            if not edge.is_manifold:
+                perimeter += edge_length
+    print(obj.name)
     total_area += obj_area
     total_length += obj_length
 print('Total area', total_area)
 print('Total length', total_length)
+print('Perimeter', perimeter)
